8873c78ff658ff996a16c06ecb1138aa02ee7861
Randel Jiao <randeljiao@gmail.com>
2019-09-17 21:08:09 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+Dog
+
+def bark
+  puts "Woof!"
+end
+
+def sit
+  puts "The Dog is sitting"
+end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+Person
+
+def talk
+  puts "Hello World!"
+end
+
+def walk
+  puts "The Person is walking"
+  
+end
+
+end
